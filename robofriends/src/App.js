a068010346ae83a845605442c417a6ce3584ff56
Mudit Singh <mudit@adobe.com>
2019-11-26 17:08:01 +0530

    Adding styling

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -19,15 +19,14 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(
-      robots => {
+    const filteredRobots = this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
       });
       console.log(filteredRobots);
 
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
