8a9bed991da38375bc07a79af6c7b607f80da797
Mudit Singh <mudit@adobe.com>
2019-11-26 21:12:31 +0530

    Updates

---
+++ precache-manifest.befb28aa51c18678b570cd1ddc582d16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d14ba6a8603569a584ccf03124f3991",
+    "url": "/muditF1/react-demo/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/muditF1/react-demo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d3e0deb67444ecbf4180",
+    "url": "/muditF1/react-demo/static/css/main.a6ad844f.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/muditF1/react-demo/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "d3e0deb67444ecbf4180",
+    "url": "/muditF1/react-demo/static/js/main.d706a9bf.chunk.js"
+  },
+  {
+    "revision": "c43229f480dad61a640e",
+    "url": "/muditF1/react-demo/static/js/runtime-main.0dcaaf02.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/muditF1/react-demo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5b6b8736237b5a5d8d9663a0dde2725c",
+    "url": "/muditF1/react-demo/static/media/bkg-img.5b6b8736.jpg"
+  }
+]);
