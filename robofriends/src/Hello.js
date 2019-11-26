43b967afc959b4eacdba2a904a61e7801206a9b1
Mudit Singh <mudit@adobe.com>
2019-11-26 11:15:57 +0530

    New Updates

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className='f1 tc'>
+                <h1>Hello World</h1>
+                <div>
+                    <p>Welcome to React</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Hello;
