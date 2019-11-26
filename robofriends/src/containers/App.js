a9145a891dbfd558120ffd987ad40e3e5f60dff1
Mudit Singh <mudit@adobe.com>
2019-11-26 20:47:20 +0530

    Adding error States

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import './App.css';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary'
 
 class App extends Component {
 
@@ -34,7 +35,9 @@ class App extends Component {
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
@@ -49,7 +52,6 @@ class App extends Component {
       .then(users => {
         this.setState({ robots: users })
       })
-    // .then(users => {})
   }
 }
 
